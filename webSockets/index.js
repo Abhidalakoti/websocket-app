d3e217a5079722af4cc05f7bff7ad8c336392e36
Abhidalakoti <dalakotiabhinav18@gmail.com>
2020-08-05 14:10:06 +0530

    Initial Commit

---
+++ webSockets/index.js
@@ -0,0 +1,45 @@
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+console.log('listening on port 8000');
+
+
+const wsServer = new webSocketServer({
+  httpServer: server
+});
+
+const clients = {};
+
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+wsServer.on('request', function (request) {
+  var userID = getUniqueID();
+  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+
+  // One can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+ 
+
+  //messaging functionality is just for testing  that websocket is working , it will be modified to "buttons onclick" event so that requisite functionality is obtained'
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ', message.utf8Data);
+
+      // broadcasting message to all connected clients
+      for(key in clients) {
+        clients[key].sendUTF(message.utf8Data);
+        console.log('sent Message to: ', clients[key]);
+      }
+    }
+  })
+});
